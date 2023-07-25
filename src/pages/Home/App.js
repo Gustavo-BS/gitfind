804a9bc85b412882b4f725bd6788538d92e99164
Gustavo Borges <gustavobprog@gmail.com>
2023-07-25 09:24:30 -0300

    added input and button

--- src/pages/Home/App.js
+++ src/pages/Home/App.js
@@ -1,10 +1,20 @@
 import Header from "../../components/Header/Header";
-
+import backgorund from "../../assets/background.png"
+import "./styles.css"
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <div className="conteudo">
+        <img src={backgorund} className="background" alt="background app"/>
+        <div className="info">
+          <div>
+            <input name="usuario" placeholder="@username"></input>
+            <button>Search</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
