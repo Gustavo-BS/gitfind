036bb4956de81f8f68b3f05d20fb6acf878a0519
Gustavo Borges <gustavobprog@gmail.com>
2023-07-25 10:48:23 -0300

    added repo and profile dynamic using fetch

--- src/pages/Home/App.js
+++ src/pages/Home/App.js
@@ -2,8 +2,31 @@ import Header from "../../components/Header/Header";
 import backgorund from "../../assets/background.png"
 import "./styles.css"
 import ItemList from "../../components/ItemList/ItemList";
+import { useState } from "react";
 
 function App() {
+
+  const [user, setUser] = useState('');
+  const [currentUser, setCurrentUser] = useState(null)
+  const [repos, setRepos] = useState(null)
+
+  const handleSetDate = async () =>{
+    const userData = await fetch(`https://api.github.com/users/${user}`);
+    const newUser = await userData.json();
+
+    if(newUser.name){
+      const {avatar_url, name, bio, login} = newUser;
+      setCurrentUser({avatar_url, name, bio, login});
+
+      const reposData = await fetch(`https://api.github.com/users/${user}/repos`);
+      const newRepos = await reposData.json();
+
+      if(newRepos.length){
+        setRepos(newRepos);
+      }
+    }
+  }
+
   return (
     <div className="App">
       <Header/>
@@ -11,22 +34,33 @@ function App() {
         <img src={backgorund} className="background" alt="background app"/>
         <div className="info">
           <div>
-            <input name="usuario" placeholder="@username"></input>
-            <button>Search</button>
+            <input 
+             name="usuario" 
+             value={user} 
+             onChange={e => setUser(e.target.value)} 
+             placeholder="@username">
+            </input>
+            <button onClick={handleSetDate}>Search</button>
           </div>
-          <div className="perfil">
-            <img src="https://avatars.githubusercontent.com/u/106408316?s=400&u=ac7c573bb1d1f0eb0c3cacaa931edc599b01e0ae&v=4" className="profile" alt="user img"></img>
-            <div>
-              <h3>Gustavo Borges</h3>
-              <span>@Gustavo-BS</span>
-              <p>Descrição</p>
+          {currentUser?.name ? (<>
+            <div className="perfil">
+              <img src={currentUser.avatar_url} className="profile" alt="user img"></img>
+              <div>
+                <h3>{currentUser.name}</h3>
+                <span>@{currentUser.login}</span>
+                <p>{currentUser.bio}</p>
+              </div>
             </div>
-          </div>
-          <hr/>
-          <div>
-            <h4 className="repositorio">Repositorios</h4>
-            <ItemList title="teste" description="teste descri"/>
-          </div>
+            <hr/>
+          </>) : null}
+          {repos?.length ? (<div>
+              <h4 className="repositorio">Repositorios</h4>
+              {repos.map(repo => (
+                <ItemList title={repo.name} description={repo.description}/>
+              ))}
+              
+            </div>) : null}
+          
         </div>
       </div>
     </div>
